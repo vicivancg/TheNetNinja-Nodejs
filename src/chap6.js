bd6aacd1aa3b00b17b514619672b58a9749a44a2
Victor Ivan Cross <vico.58@hotmail.com>
2017-08-31 21:39:23 -0500

    finalización de capitulo 6

--- src/chap6.js
+++ src/chap6.js
@@ -1,11 +1,17 @@
 //modules and require()
 
 /*
-    Declaramos una 
+    Declaramos una función en una variable llamada counter
+    In: arr (array)
+    Process: crear cadena informativa del numero de elementos
+    Out: string
 */
 
 var counter = function(arr){
     return "Hay " + arr.length +" elementos en el array";
 };
 
+/*
+    exportación de l variable counter,para ser "requerida" por otro modulo
+*/
 module.exports = counter;
--- src/chap6a.js
+++ src/chap6a.js
@@ -1,3 +1,11 @@
+/*
+    almacenamiento del objeto counter, a través de require
+*/
+
 var counter = require('./chap6');
+
+/*
+    llamada ala varible-función counter
+*/
 
 console.log(counter(["Shaun","Crystal","Ryu"]));
