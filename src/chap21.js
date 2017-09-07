db42a18a423b638d03e276212021b8b4df2a0f1c
Victor Ivan Cross <vico.58@hotmail.com>
2017-09-07 02:14:44 -0500

    Capitulo 21 finalizado

---
+++ src/chap21.js
@@ -0,0 +1,11 @@
+//Package.json
+
+/*
+    el archivo package.json contiene la información de los packetes
+    y dependencias del proyecto, puede ser inicializado con:
+
+    npm init
+
+    en el apartado de dependecies se van a ir agregando o quitando
+    las dependencias que se agreguen, de manera automatica.
+*/
