498c6ad16e33f314e94164ffd2f059c355c1a2f3
Victor Ivan Cross <vico.58@hotmail.com>
2017-09-07 02:28:22 -0500

    Capitulo 22 finalizado

---
+++ src/chap22.js
@@ -0,0 +1,22 @@
+//Installing nodemon
+
+/*
+    Nodemon permite monitorear los archivos y una vez que se ha realizado
+    un cambio se refleja, sin tener que detener la ejecucíon de la app y
+    volver a iniciarla, lo que cuando se esta desarrollando es muy utíl.
+
+    se instala con el comando
+
+    npm install -g nodemon
+
+    el parametro -g significa que se instala globalmente, no solo en el
+    proyecto.
+
+    para utilizar nodemon, ejecutamos la app con:
+    
+    nodemon archivo.js
+
+    cada que se haga un cambio, nodemon reiniciara node para que se
+    reflejen los cambios.
+
+*/
