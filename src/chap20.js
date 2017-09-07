3aeec7bd4063784484df8a75a46af1470e9c3d05
Victor Ivan Cross <vico.58@hotmail.com>
2017-09-07 01:52:37 -0500

    Capitulo 20 finalizado

---
+++ src/chap20.js
@@ -0,0 +1,23 @@
+//The node package manager NPM
+
+/*NPM es un completo ecosistema que permite la administracion,
+  acceso y publicación de packetes, haciendo facil el manejo
+  de dependecias de los mismos. (Es genial !!)
+*/
+
+/*
+    Podemos instalar por ejemplo express utilizando
+
+    npm install express
+
+    Con lo que se instalara express en el proyecto y todas sus dependencias
+    y podamos utilizarlo comodamente (si yo también amo a NPM).
+
+    También podemos eliminar paquetes, por ejemplo para eliminar express
+    utilizamos:
+
+    npm uninstall express
+
+    la información de los paquetes y su instalación puede ser consultada
+    desde la pagina de NPM.
+*/
